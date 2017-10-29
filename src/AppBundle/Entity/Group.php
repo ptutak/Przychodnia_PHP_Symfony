0539701e7901da2d9254056d187f2aecfbbbab01
ptutak <p.tutak@outlook.com>
2017-10-29 12:23:57 +0100

    users and groups

---
+++ src/AppBundle/Entity/Group.php
@@ -0,0 +1,32 @@
+<?php
+// src/AppBundle/Entity/Group.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
+ */
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+}
