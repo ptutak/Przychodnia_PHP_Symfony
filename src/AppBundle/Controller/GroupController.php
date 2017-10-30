66d60d0f3c63e52c49c91209a68ff373db375f50
ptutak <p.tutak@outlook.com>
2017-10-30 20:02:20 +0100

    Controller overrided

---
+++ src/AppBundle/Controller/GroupController.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PiotrTutak
+ * Date: 30.10.2017
+ * Time: 19:56
+ */
+
+namespace AppBundle\Controller;
+use FOS\UserBundle\Controller\GroupController as BaseController;
+use FOS\UserBundle\Event\FilterGroupResponseEvent;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseGroupEvent;
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class GroupController extends BaseController
+{
+
+    public function getExistingRoles()
+    {
+        $roleHierarchy = $this->container->getParameter('security.role_hierarchy.roles');
+        $roles = array_keys($roleHierarchy);
+        $theRoles=[];
+        foreach ($roles as $role) {
+            $theRoles[$role] = $role;
+        }
+        return $theRoles;
+    }
+
+    /**
+     * Edit one group, show the edit form.
+     *
+     * @param Request $request
+     * @param string  $groupName
+     *
+     * @return Response
+     */
+    public function editAction(Request $request, $groupName)
+    {
+        $group = $this->findGroupBy('name', $groupName);
+
+        /** @var $dispatcher EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+
+        $event = new GetResponseGroupEvent($group, $request);
+        $dispatcher->dispatch(FOSUserEvents::GROUP_EDIT_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+//        /** @var $formFactory FactoryInterface */
+        /*        $formFactory = $this->get('fos_user.group.form.factory');
+                $form = $formFactory->createForm();
+        */
+        /** @var $formFactory \FOS\UserBundle\Form\Factory\FactoryInterface */
+        $formFactory = $this->get('fos_user.group.form.factory');
+
+        $form = $formFactory->createForm();
+        $form->add('roles', ChoiceType::class, array(
+            'choices' => $this->getExistingRoles(),
+            'data' => $group->getRoles(),
+            'label' => 'Roles',
+            'expanded' => true,
+            'multiple' => true,
+            'mapped' => true,
+        ));
+
+        $form->setData($group);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var $groupManager \FOS\UserBundle\Model\GroupManagerInterface */
+            $groupManager = $this->get('fos_user.group_manager');
+
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(FOSUserEvents::GROUP_EDIT_SUCCESS, $event);
+
+            $groupManager->updateGroup($group);
+
+            if (null === $response = $event->getResponse()) {
+                $url = $this->generateUrl('fos_user_group_show', array('groupName' => $group->getName()));
+                $response = new RedirectResponse($url);
+            }
+
+            $dispatcher->dispatch(FOSUserEvents::GROUP_EDIT_COMPLETED, new FilterGroupResponseEvent($group, $request, $response));
+
+            return $response;
+        }
+
+        return $this->render('@FOSUser/Group/edit.html.twig', array(
+            'form' => $form->createView(),
+            'group_name' => $group->getName(),
+        ));
+    }
+
+}
