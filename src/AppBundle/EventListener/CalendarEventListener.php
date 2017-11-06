03e16ff271e1dd6dcf14c36b54f41e9ba91c5def
ptutak <p.tutak@outlook.com>
2017-11-06 17:54:53 +0100

    calendar

--- src/AppBundle/EventListener/CalendarEventListener.php
+++ src/AppBundle/EventListener/CalendarEventListener.php
@@ -37,8 +37,16 @@ class CalendarEventListener
         // load events using your custom logic here,
         // for instance, retrieving events from a repository
 
+        $eventEntity = new EventEntity("Moj event", new \DateTime("2017-11-07"), new \DateTime("2017-11-07"));
 
+        //optional calendar event settings
+        $eventEntity->setAllDay(true); // default is false, set to true if this is an all day event
+        $eventEntity->setBgColor('#FF0000'); //set the background color of the event's label
+        $eventEntity->setFgColor('#FFFFFF'); //set the foreground color of the event's label
+        $eventEntity->setUrl('http://www.google.com'); // url to send user to when event label is clicked
+        $eventEntity->setCssClass('my-custom-class'); // a custom class you may want to apply to event labels
 
-
+        //finally, add the event to the CalendarEvent for displaying on the calendar
+        $calendarEvent->addEvent($eventEntity);
     }
 }
